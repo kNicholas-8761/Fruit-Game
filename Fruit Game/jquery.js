3a4c7d237440605644d64aed09f8bd3a3ac42443
user <kamauri84@gmail.com>
2020-09-22 13:31:09 +1000

    change start button to reset game

--- Fruit Game/jquery.js
+++ Fruit Game/jquery.js
@@ -6,20 +6,30 @@ $(function () {
 
   $("#startreset").click(function () {
     if (playing == true) {
+
       //reload page
+
       location.reload();
     } else {
+
       //we are not playing
+
       playing = true; //game initiated
 
       //set score to 0
+
       score = 0; //set score to 0
       $("#scorevalue").html(score);
 
       //show trials left
+
       $("#trialsLeft").show();
       trialsLeft = 3;
       addHearts()
+
+   //change button text to reset game
+   $("#startreset").html("Reset Game");
+
     }
   });
   function addHearts(){
